7a270400032855fa1afdfc85fb26bf03529558fa
soyMA05 <miancas05sd@gmail.com>
2022-06-02 23:56:04 -0500

    Creando metodos http en el controlador

--- ServicioSOA/ServicioSOA/Controllers/InventarioController.cs
+++ ServicioSOA/ServicioSOA/Controllers/InventarioController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServicioSOA.Modelo;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +9,58 @@ namespace ServicioSOA.Controllers
     [ApiController]
     public class InventarioController : ControllerBase
     {
-        // GET: api/<InventarioController>
+        // GET: api/<InventarioController> Interfaz para interactuar con la clase Producto
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Producto> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Program.productos;
         }
 
-        // GET api/<InventarioController>/5
+        // GET api/<InventarioController>/5  Para buscar un producto
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Producto Get(int id)
         {
-            return "value";
+            return Program.productos.Find(producto => producto.id==id);
         }
 
-        // POST api/<InventarioController>
+        // POST api/<InventarioController> Para crear un nuevo producto
         [HttpPost]
-        public void Post([FromBody] string value)
+        public bool Post(Producto producto)
         {
+            int pos = Program.productos.FindIndex(item => item.id == producto.id);
+            if (pos == -1)
+            {
+                Program.productos.Add(producto);
+                return true;
+            }
+            return false;
         }
 
-        // PUT api/<InventarioController>/5
+        // PUT api/<InventarioController>/5 Modificar un producto
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public bool Put(int id, Producto producto)
         {
+            int pos = Program.productos.FindIndex(item => item.id == producto.id);
+            if (pos == -1)
+            {
+                Program.productos.RemoveAt(pos);
+                Program.productos.Add(producto);
+                return true;
+            }
+            return false;
         }
 
-        // DELETE api/<InventarioController>/5
+        // DELETE api/<InventarioController>/5 Eliminar un producto
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public bool Delete(int id)
         {
+            int pos = Program.productos.FindIndex(item => item.id == id);
+            if (pos == -1)
+            {
+                Program.productos.RemoveAt(pos);
+                return true;
+            }
+            return false;
         }
     }
 }
