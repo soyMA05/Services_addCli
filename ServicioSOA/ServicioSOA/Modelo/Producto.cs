79e1dbf4286b36daed3cee6dea259a7a50421561
soyMA05 <miancas05sd@gmail.com>
2022-06-02 22:55:23 -0500

    Creando servicios soap

---
+++ ServicioSOA/ServicioSOA/Modelo/Producto.cs
@@ -0,0 +1,21 @@
+﻿namespace ServicioSOA.Modelo
+{
+    public class Producto
+    {
+        //atributos
+        public int id {get;set;}
+        public string nombre {get;set;}
+        public decimal pvp {get;set;}
+        public int cantidad {get;set;}
+
+        //constructor
+        public Producto(int id, string nombre, decimal pvp, int cantidad)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.pvp = pvp;
+            this.cantidad = cantidad;
+        }
+
+    }
+}
