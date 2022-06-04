68ae761fe617ae9fbd11cc0b21448f99bed2f67c
soyMA05 <miancas05sd@gmail.com>
2022-06-03 22:24:43 -0500

    Creando cliente para el consumo de servicios

---
+++ AppClienteConsola/AppClienteConsola/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+*/
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppClienteConsola
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string url = "http://localhost:9090/api/inventario";
+            
+        }
+        static void ListarElementos(string url)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                using (HttpRequestMessage solicitud = new HttpRequestMessage())
+                {
+                    solicitud.RequestUri = new Uri(url);   
+                    solicitud.Method = HttpMethod.Get;
+
+                    //solicitudes
+                    solicitud.Headers.Add("Accept", "application/json");
+
+                    //respuesta de la solicitudd
+                    Task<HttpResponseMessage> httpResponse = cliente.SendAsync(solicitud);
+                    using (HttpResponseMessage respuesta = httpResponse.Result)
+                    {
+                        //obtener contenido (puede ser stream o texto, y si es necesario transformarlo a objetos)
+                        Console.WriteLine(respuesta.ToString());
+                        HttpStatusCode codigo = respuesta.StatusCode;
+                        Console.WriteLine("Estado {0}", codigo);  
+                        Console.WriteLine("codigo {0}", (int)codigo);
+                        HttpContent contenido = respuesta.Content;
+                        Task<string> datos =contenido.ReadAsStringAsync();
+                        string cadena = datos.Result;
+                        Console.WriteLine(cadena);
+                    }
+
+                }
+            }
+        }
+    }
+}
